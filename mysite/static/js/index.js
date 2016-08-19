af97246dec853c9835654ce2f379a73b3715fbf4
happyball <yangchiyi10@gmail.com>
2016-08-19 18:09:02 +0800

    add damage animation

---
+++ mysite/static/js/index.js
@@ -0,0 +1,44 @@
+pos_count = 0;
+pos_x = 350;
+pos_y = 250;
+
+$(document).ready(function(){
+	/*svg = d3.select(".main-layout").append("svg").attr("id", "main-svg");
+	svg.append("rect").attr({
+		'width': 400,
+		'height': 200,
+		'fill': 'blue',
+		'x': 300,
+		'y': 100,
+		'id': 'rect-btn',
+		'class': 'btn'
+	});
+
+	window.setInterval(function(){
+		// var parentOffset = $("#rect-btn").parent().offset(); 
+		if (pos_count%10 == 0){
+			if (pos_count == 0) pos_x = 350;
+			else pos_x += 150;
+		}
+		txt = svg.append("text").attr({
+			'x': pos_x,
+			'y': pos_y
+		})
+		.style("font-size", "20px")
+		.text("100")
+		.transition().duration(1000)
+		.attr({
+			"y": pos_y - 100
+		})
+		.style("opacity", 0);
+
+		txt.remove();
+
+		if (pos_count == 29) pos_count = 0;
+		else pos_count++;
+
+	}, 100);*/
+
+
+});
+
