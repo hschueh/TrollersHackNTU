c2ded2fbfb5762a76fdcd3123a221d7ed94e9c30
happyball <yangchiyi10@gmail.com>
2016-08-21 02:41:34 +0800

    finish charge and income pages

--- mysite/charge/views.py
+++ mysite/charge/views.py
@@ -56,7 +56,7 @@ def charge(request):
 def date_changed(request, chargestate):
     if request.method == "POST":
         post_dict = request.POST.dict()
-        date = datetime(post_dict["yr"],post_dict["mon"],post_dict["day"])
+        date = datetime.datetime(int(post_dict["yr"]),int(post_dict["mon"]),int(post_dict["day"]))
         user = User.objects.get(id=request.user.id)
         records = Record.objects.filter(user_id=user.id)
 
