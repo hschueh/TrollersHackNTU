36239e9e2b2294d79017e1096830d15b77242cbf
happyball <yangchiyi10@gmail.com>
2016-08-21 03:19:49 +0800

    implementing tasks check

--- mysite/static/js/missions.js
+++ mysite/static/js/missions.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+	$.get("/missions/", function(data){
+		console.log(data);
+	});
+
 	var days = $('#days').attr("value");
 	var rdays = $('#rdays').attr("value");
 
@@ -7,20 +11,20 @@ $(document).ready(function(){
     
 	$('#daysbar').text(Math.round(days/rdays*100)+"%");
 
-	var valeur = days/rdays*100;
-	$('#daysbar').css('width', valeur+'%').attr('aria-valuenow', valeur);
+	var valuer = days/rdays*100;
+	$('#daysbar').css('width', valuer+'%').attr('aria-valuenow', valuer);
 
 
 	
-    var used = 1500;
-    var budget = 10000;
-    //var budget = $('#task_budget').attr("value");
+    var used = 100;
+    // var budget = 10000;
+    var budget = $('#task_budget').attr("value");
 
     
 	$('#budget_bar').text(Math.round(used/budget*100)+"%");
 
-	valeur = used/budget*100;
-	$('#budget_bar').css('width', valeur+'%').attr('aria-valuenow', valeur);
+	valuer = used/budget*100;
+	$('#budget_bar').css('width', valuer+'%').attr('aria-valuenow', valuer);
 
 
 
@@ -32,8 +36,8 @@ $(document).ready(function(){
     
 	$('#consume_bar').text(Math.round(current/required*100)+"%");
 
-	valeur = current/required*100;
-	$('#consume_bar').css('width', valeur+'%').attr('aria-valuenow', valeur);
+	valuer = current/required*100;
+	$('#consume_bar').css('width', valuer+'%').attr('aria-valuenow', valuer);
 
 	
 
