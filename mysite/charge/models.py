507c9bba64781cf8b2edffab57ae581db0eed92e
David32626 <b98901026@ntu.edu.tw>
2016-08-20 17:03:11 +0800

    add mission name and user gender

--- mysite/charge/models.py
+++ mysite/charge/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
     dps = models.DecimalField(max_digits=5,decimal_places=0,default=1)
     facebookID = models.CharField(max_length = 100,default="")
     token = models.CharField(max_length = 200,default="")
+    gender = models.BooleanField(default=True)
 
 class Category(models.Model):
     name = models.CharField(max_length = 100)
@@ -54,6 +55,7 @@ class User_Monster(models.Model):
     createTime = models.DateTimeField(auto_now_add=True, blank=True)
 
 class Missions(models.Model):
+    name = models.CharField(max_length = 100,default="")
     user = models.ForeignKey(User,on_delete=models.CASCADE,)
     missionType = models.CharField(max_length = 40)
     status = models.CharField(max_length = 20)
