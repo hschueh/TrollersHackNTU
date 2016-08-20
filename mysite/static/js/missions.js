9b75c9ca3cb0e55600a309c6d4c7fd1d3ad847d0
arthurthebrave <b98901110@ntu.edu.tw>
2016-08-21 02:39:07 +0800

    mission_consecutive

--- mysite/static/js/missions.js
+++ mysite/static/js/missions.js
@@ -4,12 +4,40 @@ $(document).ready(function(){
 	var rdays = $('#rdays').attr("value");
 
 
-    console.log(days);
-    console.log(rdays);
-    console.log(days/rdays);
+    
 	$('#daysbar').text(Math.round(days/rdays*100)+"%");
 
 	var valeur = days/rdays*100;
 	$('#daysbar').css('width', valeur+'%').attr('aria-valuenow', valeur);
+
+});
+
+$(document).ready(function(){
+
+	var days = $('#con_bud_days').attr("value");
+	var rdays = $('#con_bud_rdays').attr("value");
+    var used = 1500;
+    var budget = 10000;
+    //var budget = $('#task_budget').attr("value");
+
+    
+	$('#budget_bar').text(Math.round(used/budget*100)+"%");
+
+	var valeur = used/budget*100;
+	$('#budget_bar').css('width', valeur+'%').attr('aria-valuenow', valeur);
+
+});
+
+$(document).ready(function(){
+
+	var current = $('#con_con_c').attr("value");
+	var required = $('#con_con_r').attr("value");
+    console.log(current);
+    console.log(required);
+    
+	$('#consume_bar').text(Math.round(current/required*100)+"%");
+
+	var valeur = current/required*100;
+	$('#consume_bar').css('width', valeur+'%').attr('aria-valuenow', valeur);
 
 });
