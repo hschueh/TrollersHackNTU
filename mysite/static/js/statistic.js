3bd46ef7c9347aafd3a6aa23c781046355da9fc3
happyball <yangchiyi10@gmail.com>
2016-08-20 21:02:18 +0800

    change income charge page direct method

--- mysite/static/js/statistic.js
+++ mysite/static/js/statistic.js
@@ -1,15 +1,34 @@
 $(document).ready(function(){
 	var charge_state = $("#chargestate-var").attr("value");
+
 	
-	$.get("/statistic_data/",function(data){
+	$.get("/statistic_data/" + charge_state,function(data){
 		var width = 370, height = 350, radius = Math.min(width,height)/2;
 
 		console.log(data);
+
+		var color = d3.scale.ordinal()
+		    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+		var arc = d3.svg.arc()
+		    .outerRadius(radius - 10)
+		    .innerRadius(0);
+
+		var labelArc = d3.svg.arc()
+		    .outerRadius(radius - 40)
+		    .innerRadius(radius - 40);
+
+		var pie = d3.layout.pie()
+		    .sort(null)
+		    .value(function(d) { return d.money; });
+
 		svg = d3.select("div.graph-block").append("svg").attr({
 			"id": "main-graph",
 			"width": width,
 			"height": height
-		});
+		})
+		.append("g")
+    	.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 
 	});
